d800e0adb1fb53d4e2d079c82489d8b0c3f89c19
thomas utsey <tomutsey@yahoo.com>
2020-01-12 23:38:44 -0500

    Build Out initial prelim framework

---
+++ src/Components/imageList.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ImageList = () =>{
+    return(
+        <div>This is the image List component</div>
+    )
+}
+
+export default ImageList
---
+++ src/Components/searchBar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SearchBar = () =>{
+    return(
+        <div>This is the search bar component</div>
+    )
+}
+
+export default SearchBar
