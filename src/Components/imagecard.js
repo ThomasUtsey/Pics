a265522ecc280f45a552c08cca6fcc21b1cc0501
thomas utsey <tomutsey@yahoo.com>
2020-01-14 08:32:04 -0500

    React practice

--- src/Components/imagecard.js
+++ src/Components/imagecard.js
@@ -1,14 +1,31 @@
 import React from 'react'
+import './imageList.css'
 
 class ImageCard extends React.Component{
+constructor(props){
+    super(props)
+    this.state = {
+        span:0
+    }
+    this.imageRef = React.createRef()
+}
+componentDidMount(){
+    this.imageRef.current.addEventListener('load',this.setSpan)
+}
+setSpan = () =>{
+const height = (this.imageRef.current.clientHeight)
+const span = Math.ceil(height/10)+1
+this.setState({span:span})
+}
 
 render(){
     const{description,urls} = this.props.image
-    
+
     return(
-    <div className="image-card">
+    <div style={{gridRowEnd:`span ${this.state.span}`}} className="image-card">
         
         <img 
+        ref={this.imageRef}
         src={urls.regular} 
         alt={description}
         />
